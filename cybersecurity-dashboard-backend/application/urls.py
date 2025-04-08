e712207f56fef40b65a43baa7fb7dc4a498dec96
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 20:28:11 +0300

    urls

--- cybersecurity-dashboard-backend/application/urls.py
+++ cybersecurity-dashboard-backend/application/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrganizationViewSet, EmployeeViewSet, ThreatUpdateViewSet, EducationalResourceViewSet
+from .views import OrganizationViewSet, EmployeeViewSet, ThreatUpdateViewSet, EducationalResourceViewSet, RegisterView, LoginView
 
 router = DefaultRouter()
 router.register(r'organizations', OrganizationViewSet)
 router.register(r'employees', EmployeeViewSet)
 router.register(r'threat-updates', ThreatUpdateViewSet)
 router.register(r'educational-resources', EducationalResourceViewSet)
+router.register(r'register', RegisterView, basename='register')
+router.register(r'login', LoginView, basename='login')
 
 urlpatterns = [
     path('', include(router.urls)),
