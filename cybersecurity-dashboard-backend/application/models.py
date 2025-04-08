2d8583a3a0b7c5823b90af54a12fead8d15147a1
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 20:13:43 +0300

    models

--- cybersecurity-dashboard-backend/application/models.py
+++ cybersecurity-dashboard-backend/application/models.py
@@ -1,5 +1,10 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    is_master_admin = models.BooleanField(default=False)
+    is_organization_admin = models.BooleanField(default=False)
 
 class Organization(models.Model):
     name = models.CharField(max_length=128)
