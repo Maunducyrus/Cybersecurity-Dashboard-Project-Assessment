10a39a64e6fbcabfdce39180c568aac7546621a7
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 20:18:11 +0300

    serializers

--- cybersecurity-dashboard-backend/application/serializers.py
+++ cybersecurity-dashboard-backend/application/serializers.py
@@ -1,5 +1,46 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Organization, Employee, ThreatUpdate, EducationalResource
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_master_admin', 'is_organization_admin')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'is_master_admin', 'is_organization_admin')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            is_master_admin=validated_data.get('is_master_admin', False),
+            is_organization_admin=validated_data.get('is_organization_admin', False)
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
+
+    def get_token(self, obj):
+        user = User.objects.get(username=obj['username'])
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+# models serializers
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
