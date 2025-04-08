a62692b403a7ecce4ea3760a3a8b4e35b097def1
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 17:58:28 +0300

    views.py

--- cybersecurity-dashboard-backend/application/views.py
+++ cybersecurity-dashboard-backend/application/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Organization, Employee, ThreatUpdate, EducationalResource
+from .serializers import OrganizationSerializer, EmployeeSerializer, ThreatUpdateSerializer, EducationalResourceSerializer
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class ThreatUpdateViewSet(viewsets.ModelViewSet):
+    queryset = ThreatUpdate.objects.all()
+    serializer_class = ThreatUpdateSerializer
+
+class EducationalResourceViewSet(viewsets.ModelViewSet):
+    queryset = EducationalResource.objects.all()
+    serializer_class = EducationalResourceSerializer
