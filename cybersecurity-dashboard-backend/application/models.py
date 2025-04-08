a21da784723607385b1b262056f7ae9e3ecb1722
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 17:54:47 +0300

    models

--- cybersecurity-dashboard-backend/application/models.py
+++ cybersecurity-dashboard-backend/application/models.py
@@ -1,3 +1,36 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=128)
+    subscription_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=128)
+    organization = models.ForeignKey(Organization, related_name='employees', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class ThreatUpdate(models.Model):
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message[:50]
+
+class EducationalResource(models.Model):
+    RESOURCE_TYPES = [
+        ('video', 'Video'),
+        ('blog', 'Blog'),
+    ]
+
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    resource_type = models.CharField(max_length=50, choices=RESOURCE_TYPES)
+
+    def __str__(self):
+        return self.title
