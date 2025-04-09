258bbbc9cf98649ede087e392c281eded39c2fa7
Maunducyrus <maunducyrus123@gmail.com>
2025-04-09 22:57:11 +0300

    deletion

--- cybersecurity-dashboard-backend/application/views.py
+++ cybersecurity-dashboard-backend/application/views.py
@@ -32,18 +32,7 @@ class RegisterView(viewsets.ViewSet):
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginView(viewsets.ViewSet):
 
-    def create(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = User.objects.get(username=serializer.validated_data['username'])
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            })
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     # permission_classes = [AllowAny]
 
     # def create(self, request):
