9b4442eea089a5d6874af55439776df289072623
Maunducyrus <maunducyrus123@gmail.com>
2025-04-10 09:42:14 +0300

    Header.js

---
+++ cybersecurity-dashboard-frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Header = () => {
+  return (
+    <AppBar position="fixed" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Cybersecurity Dashboard
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
